a2c57b13ccb46ad3f59b7c708186a87957ed55fd
IronMonger13 <ishaanbhalla632@gmail.com>
2025-06-16 03:56:02 +0530

    Completed fetching data from db and deleteing data from db and setting up the endpoints to add, fetch or delete from database

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
-const { addIncome } = require("../controllers/Income");
+const { addIncome, getIncome, deleteIncome } = require("../controllers/Income");
 
-router.post("/addIncome", addIncome);
+router.post("/addIncome", addIncome).get("/getIncome", getIncome).delete("/deleteIncome/:id", deleteIncome);
 
 module.exports = router;
