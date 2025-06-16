c108cf834c34e17bba370be70806d4076b20aa14
IronMonger13 <ishaanbhalla632@gmail.com>
2025-06-16 07:24:52 +0530

    Made navigation section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,23 @@
+import { useMemo, useState } from "react";
 import styled from "styled-components";
 import bg from "./img/bg.png";
 import { MainLayout } from "./Styles/Layouts";
 import Orb from "./Components/Orb/Orb";
+import Navigation from "./Components/Navigation/Navigation";
 
 function App() {
+    const [active, setActive] = useState(1);
+
+    const orbMemo = useMemo(() => {
+        return <Orb></Orb>;
+    }, []);
+
     return (
         <AppStyled bg={bg} className="App">
-            <Orb></Orb>
-            <MainLayout></MainLayout>
+            {orbMemo}
+            <MainLayout>
+                <Navigation active={active} setActive={setActive}></Navigation>
+            </MainLayout>
         </AppStyled>
     );
 }
--- frontend/src/Components/Orb/Orb.jsx
+++ frontend/src/Components/Orb/Orb.jsx
@@ -2,34 +2,40 @@ import React from "react";
 import styled, { keyframes } from "styled-components";
 import { useWindowSize } from "../../Utils/useWindowSize";
 
+// Function to generate keyframes using props
+const generateMoveOrb = (x, y) => keyframes`
+  0% {
+    transform: translate(0, 0);
+  }
+  50% {
+    transform: translate(${x}px, ${y}px);
+  }
+  100% {
+    transform: translate(0, 0);
+  }
+`;
+
+const OrbStyled = styled.div`
+    width: 70vh;
+    height: 70vh;
+    position: absolute;
+    top: 0;
+    left: 0;
+    border-radius: 50%;
+    background: linear-gradient(180deg, #f56692 0%, #f2994a 100%);
+    filter: blur(400px);
+
+    animation: ${({ x, y }) => generateMoveOrb(x, y)} 15s alternate linear infinite;
+`;
+
 const Orb = () => {
     const { width, height } = useWindowSize();
-    console.log(width, height);
 
-    const moveOrb = keyframes`
-        0%{
-            transform: translate(0, 0);
-        }
-        50%{
-            transform: translate(${width / 1.2}px, ${height / 2}px);
-        }
-        100%{
-            transform: translate(0, 0);
-        }
-    `;
+    // fallback values to avoid NaN during first render
+    const x = width ? width / 1.5 : 1000;
+    const y = height ? height / 2 : 300;
 
-    const OrbStyled = styled.div`
-        width: 70vh;
-        height: 70vh;
-        position: absolute;
-        border-radius: 50%;
-        margin-top: -37vh;
-        margin-left: -37vh;
-        background: linear-gradient(180deg, #f56692 0%, #f2994a 100%);
-        filter: blur(400px);
-        animation: ${moveOrb} 15s alternate linear infinite;
-    `;
-    return <OrbStyled></OrbStyled>;
+    return <OrbStyled x={x} y={y} />;
 };
 
 export default Orb;
