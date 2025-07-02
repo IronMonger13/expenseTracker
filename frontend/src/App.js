9ceb8773fc0af14a277c3f711cb705cfaf7ddfe4
IronMonger13 <ishaanbhalla632@gmail.com>
2025-07-02 06:56:02 +0530

    Added global context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,31 @@ import bg from "./img/bg.png";
 import { MainLayout } from "./Styles/Layouts";
 import Orb from "./Components/Orb/Orb";
 import Navigation from "./Components/Navigation/Navigation";
+import Dashboard from "./Components/Dashboard/Dashboard";
+import Incomes from "./Components/Incomes/Incomes";
+import Expenses from "./Components/Expenses/Expenses";
+import { GlobalContext, useGlobalContext } from "./Context/GlobalContext";
 
 function App() {
     const [active, setActive] = useState(1);
+
+    const global = useGlobalContext();
+    console.log(useGlobalContext)
+
+    const displayData = () => {
+        switch (active) {
+            case 1:
+                return <Dashboard></Dashboard>;
+            case 2:
+                return <Dashboard></Dashboard>;
+            case 3:
+                return <Incomes></Incomes>;
+            case 4:
+                return <Expenses></Expenses>;
+            default:
+                return <Dashboard></Dashboard>;
+        }
+    };
 
     const orbMemo = useMemo(() => {
         return <Orb></Orb>;
@@ -17,7 +39,7 @@ function App() {
             {orbMemo}
             <MainLayout>
                 <Navigation active={active} setActive={setActive}></Navigation>
-                <main></main>
+                <main>{displayData()}</main>
             </MainLayout>
         </AppStyled>
     );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { GlobalStyle } from "./Styles/GlobalStyle";
+import { GlobalProvider } from "./Context/GlobalContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <GlobalStyle></GlobalStyle>
-        <App />
+        <GlobalStyle />
+        <GlobalProvider>
+            <App />
+        </GlobalProvider>
     </React.StrictMode>
 );
